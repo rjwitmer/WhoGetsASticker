5c1545dfa06ae8bf130c2e00d98b4ec78dcf6434
Bob Witmer <bwitmer@golden.net>
2023-07-05 11:02:10 -0400

    Initial Commit

---
+++ WhoGetsASticker/WhoGetsAStickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoGetsAStickerApp.swift
+//  WhoGetsASticker
+//
+//  Created by Bob Witmer on 2023-07-05.
+//
+
+import SwiftUI
+
+@main
+struct WhoGetsAStickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
